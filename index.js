01f1813cacfb1c3b0aa7dc7f21b6d421438ca3d7
MohammadAli <mh.newday@gmail.com>
2022-02-02 23:47:48 +0330

    Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-with, Content-Type, Accept');
+    next();
+})
 
 //Configuration
 console.log("Application Name : " + config.get("name"));
